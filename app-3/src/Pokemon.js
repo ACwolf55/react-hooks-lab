3669606c95d4df0ee19b40da88cba2b130346d74
Alex Crowe <alexcwolf5@gmail.com>
2021-02-01 22:12:31 -0700

    all apps working

---
+++ app-3/src/Pokemon.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const Pokemon = (props) =>{
+    const {name} = props.match.params
+    const [pokemon,setPokemon] = useState({})
+
+
+    useEffect( ()=>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res)=>{
+            setPokemon(res.data)
+            console.log(res.data.sprites.versions[0])
+        })
+    },[name])
+
+return(
+    <p>
+        <h1>{pokemon.name}</h1>
+        <img alt={pokemon.name} src={pokemon.sprites?.front_default}/> 
+        {/* <img alt={pokemon.name} src={pokemon.sprites.versions?."generation_i"?."red_blue".front_default}/> */}
+    </p>
+)
+}
+export default Pokemon
